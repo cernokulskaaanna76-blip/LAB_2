4e76ec54f74a69f0ed6c97d0c633be1033bf3ab3
LucasKane29 <58682387+LucasKane29@users.noreply.github.com>
2026-03-19 10:53:36 +0200

    code review #5

--- lr2-api/src/services/user.service.js
+++ lr2-api/src/services/user.service.js
@@ -1,5 +1,5 @@
 const userRepository = require('../repository/user.repository');
-
+//Та сама проблема відсутність асинхронності та обробки помилок
 class UserService {
     
     getAllUsers() {
@@ -8,11 +8,12 @@ class UserService {
 
     
     registerUser(userData) {
+        //Не вистачає валідації самого userData якщо це буде пуста строка то далі перевірка викличе помилку
         if (!userData.name) {
             throw new Error("Ім'я користувача обов'язкове!");
         }
         return userRepository.create(userData);
     }
 }
-
+//Проблема з сінглтоном
 module.exports = new UserService();
