c489a8b31659490a84b4dfe149c997e1ed94f91b
LucasKane29 <58682387+LucasKane29@users.noreply.github.com>
2026-03-19 11:02:38 +0200

    code review #6

--- lr2-api/src/repository/shift.repository.js
+++ lr2-api/src/repository/shift.repository.js
@@ -2,13 +2,15 @@ const shifts = [
     { id: 1, userId: 1, date: '2026-03-19', type: 'morning' },
     { id: 2, userId: 2, date: '2026-03-19', type: 'evening' }
 ];
-
+//відсутність асінхронності, базових методів findById, delete, update
 class ShiftRepository {
-    findAll() { return shifts; }
+    findAll() { return shifts; } // краще зробити return [...shifts];
+// для сутності user була зроблена DTO, чому для shift тоді передається сюди сирий реквест. Так само треба зробити DTO та мапити дані
     create(data) {
-        const newShift = { id: shifts.length + 1, ...data };
+        const newShift = { id: shifts.length + 1, ...data }; // краще зробити через const newId = shifts.length > 0 ? Math.max(...shifts.map(s => s.id)) + 1: 1; бо можуть бути колізії при видаленні записів
         shifts.push(newShift);
         return newShift;
     }
 }
+//сінглтон
 module.exports = new ShiftRepository();
